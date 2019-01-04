0eae54c902199e63901d3724fa8f2fa5271ad500
Jud White <github@judsonwhite.com>
2019-01-04 00:13:11 -0500

    test: move helper functions to svc_windows_test.go

--- svc/svc_common_test.go
+++ svc/svc_common_test.go
@@ -41,35 +41,3 @@ func makeProgram(startCalled, stopCalled, initCalled *int) *mockProgram {
 		},
 	}
 }
-
-func equal(t *testing.T, expected, actual interface{}) {
-	if !reflect.DeepEqual(expected, actual) {
-		_, file, line, _ := runtime.Caller(1)
-		t.Logf("\033[31m%s:%d:\n\n\t   %#v (expected)\n\n\t!= %#v (actual)\033[39m\n\n",
-			filepath.Base(file), line, expected, actual)
-		t.FailNow()
-	}
-}
-
-func assertNil(t *testing.T, object interface{}) {
-	if !isNil(object) {
-		_, file, line, _ := runtime.Caller(1)
-		t.Logf("\033[31m%s:%d:\n\n\t   <nil> (expected)\n\n\t!= %#v (actual)\033[39m\n\n",
-			filepath.Base(file), line, object)
-		t.FailNow()
-	}
-}
-
-func isNil(object interface{}) bool {
-	if object == nil {
-		return true
-	}
-
-	value := reflect.ValueOf(object)
-	kind := value.Kind()
-	if kind >= reflect.Chan && kind <= reflect.Slice && value.IsNil() {
-		return true
-	}
-
-	return false
-}
--- svc/svc_windows_test.go
+++ svc/svc_windows_test.go
@@ -443,3 +443,36 @@ func testSignalNotify(t *testing.T, signal os.Signal, sig ...os.Signal) {
 	equal(t, 1, initCalled)
 	equal(t, 0, len(wsf.changes))
 }
+
+
+func equal(t *testing.T, expected, actual interface{}) {
+	if !reflect.DeepEqual(expected, actual) {
+		_, file, line, _ := runtime.Caller(1)
+		t.Logf("\033[31m%s:%d:\n\n\t   %#v (expected)\n\n\t!= %#v (actual)\033[39m\n\n",
+			filepath.Base(file), line, expected, actual)
+		t.FailNow()
+	}
+}
+
+func assertNil(t *testing.T, object interface{}) {
+	if !isNil(object) {
+		_, file, line, _ := runtime.Caller(1)
+		t.Logf("\033[31m%s:%d:\n\n\t   <nil> (expected)\n\n\t!= %#v (actual)\033[39m\n\n",
+			filepath.Base(file), line, object)
+		t.FailNow()
+	}
+}
+
+func isNil(object interface{}) bool {
+	if object == nil {
+		return true
+	}
+
+	value := reflect.ValueOf(object)
+	kind := value.Kind()
+	if kind >= reflect.Chan && kind <= reflect.Slice && value.IsNil() {
+		return true
+	}
+
+	return false
+}
